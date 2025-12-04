57267d08df02340d957dccd70cb01424874c7214
RCYGM <jurgenmuller997@gmail.com>
2025-12-04 17:00:43 +0100

    feat: lp 2

--- src/components/LandigPage/index.tsx
+++ src/components/LandigPage/index.tsx
@@ -1,4 +1,5 @@
 import type React from "react";
+import demo from "./demo2.json";
 import style from "./index.module.scss";
 
 type VentajasType = string;
@@ -10,21 +11,7 @@ interface LandingPageType {
   ventajas: VentajasType[];
 }
 const LandingPage: React.FC = () => {
-  const landingPage: LandingPageType = {
-    title: "Tu negocio podría estar vendiendo el doble",
-    titleP:
-      "Empieza con una landing optimizada que convierte visitas en clientes",
-    subtitle: "Reserva una asesoría 100% gratuita donde analizamos tu negocio",
-    subtitleP:
-      "detectamos tus fugas de conversión y te mostramos el embudo exacto que necesitas",
-    ventajas: [
-      "Descubrirás por qué tu web actual no está convirtiendo (y cómo arreglarlo)",
-      "Te mostraré la estructura de landing exacta que usan negocios que venden 24/7",
-      "Identificaremos tu propuesta de valor ganadora para atraer clientes listos para comprar",
-      "Obtendrás claridad sobre qué elementos debes tener para generar confianza inmediata",
-      "Te daré un plan concreto para escalar tu adquisición de clientes sin depender de recomendaciones",
-    ],
-  };
+  const landingPage: LandingPageType = demo;
   const handeltWhatsApp = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
     e.stopPropagation();
@@ -51,13 +38,16 @@ const LandingPage: React.FC = () => {
           {landingPage.subtitleP}
         </p>
       </section>
-
+      {/* CTA Button */}
+      <button onClick={handeltWhatsApp} className={style.landing_page__cta}>
+        Quiero mi asesoría gratuita ahora →
+      </button>
       {/* Ventajas Section */}
       <section className={style.landing_page__ventajas}>
         {landingPage.ventajas.map((ventaja, index) => {
           return (
             <div key={index} className={style.landing_page__ventaja_item}>
-              <p className={style.landing_page__ventaja_text}>{ventaja}</p>
+              <p>{ventaja}</p>
             </div>
           );
         })}
